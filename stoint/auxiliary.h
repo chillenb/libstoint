5c3951566e6b2f45d36733e34ad38ca98f303f11
Christopher Hillenbrand <chillenbrand15@gmail.com>
2022-04-07 00:48:08 -0400

    one center overlap

--- stoint/auxiliary.h
+++ stoint/auxiliary.h
@@ -4,15 +4,23 @@
 #include <cmath>
 
 #define FACTORIAL(N) tgamma(N+1)
+using std::pow;
+using std::sqrt;
 
 template<int n>
 inline double NormCoef(double zeta) {
-    return std::pow(2*zeta, 0.5*(2*n+1)) / std::sqrt(FACTORIAL(2*n));
+    return pow(2*zeta, 0.5*(2*n+1)) / sqrt(FACTORIAL(2*n));
 }
 
 template<int n1, int n2, int n3, int n4>
 inline double QNormCoef(double zeta1, double zeta2, double zeta3, double zeta4) {
     return NormCoef<n1>(zeta1) * NormCoef<n2>(zeta2) * NormCoef<n3>(zeta3) * NormCoef<n4>(zeta4);
+}
+
+template<int n1, int n2>
+double OneCenterOverlap(double zeta1, double zeta2) {
+    return (pow(2,1 + n1 + n2)*FACTORIAL(n1 + n2))/(sqrt(FACTORIAL(2*n1))*sqrt(FACTORIAL(2*n2))) *
+        pow(zeta1,0.5 + n1)*pow(zeta2, 0.5 + n2)*pow(zeta1 + zeta2,-1 - n1 - n2);
 }
 
 
--- test/tests.cc
+++ test/tests.cc
@@ -1,16 +1,29 @@
 #include "stoint/auxiliary.h"
 #include <iostream>
 #include <gtest/gtest.h>
+#include <cmath>
+
+#define EXPECT_RELNEAR(A, B, reltol) EXPECT_NEAR(A, B, std::abs((B)*(reltol)))
 
 
-TEST(NormCoefTest, CheckWorking)
+using std::sqrt;
+using std::pow;
+
+TEST(NormCoefTest, SanityCheck)
 {
   EXPECT_DOUBLE_EQ(NormCoef<2>(0.5), 0.2041241452319315);
 }
 
-TEST(QNormCoefTest, CheckTwo)
+TEST(QNormCoefTest, CheckVals)
 {
   EXPECT_DOUBLE_EQ( (QNormCoef<2,2,4,4>(0.5,0.5,0.3,0.3)), 1.0414285714285717E-8 );
+}
+
+TEST(OneCenterOverlapTest, CheckVals)
+{
+  EXPECT_RELNEAR( (OneCenterOverlap<2,3>(0.5,1.0)), 512.0/729.0 * sqrt(5.0/3.0), 1e-14);
+  EXPECT_RELNEAR( (OneCenterOverlap<1,1>(4.0,6.0)), (48*sqrt(6))/125., 1e-14);
+  EXPECT_RELNEAR( (OneCenterOverlap<10,12>(0.34,0.63)), 0.57753320411680154979, 1e-14);
 }
 
 int main(int argc, char **argv)
